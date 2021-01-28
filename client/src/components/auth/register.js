9c35205893aaee24afb9ddf994c73865d2e6d5c2
Askar Talantbek <murutchan@gmail.com>
2021-01-28 13:22:27 -0600

    redux store setup

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,7 +1,8 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
-const Register = () => {
+import { setAlert } from "../../actions/alert";
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -16,7 +17,7 @@ const Register = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Password do not match");
+      props.setAlert("Password do not match", "danger");
     } else {
       console.log("SUCCESS");
     }
@@ -83,4 +84,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default connect(null, { setAlert })(Register);
