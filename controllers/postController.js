ef68e98c4bbf77234310646f1da309cbca583943
Askar Talantbek <murutchan@gmail.com>
2021-01-27 18:35:24 -0600

    likes and comments functionalities are done

--- controllers/postController.js
+++ controllers/postController.js
@@ -95,3 +95,121 @@ exports.deletePost = async (req, res) => {
     res.status(404).json({ msg: "server error" });
   }
 };
+
+//TODO:
+//@route PUT api/posts/like/:id
+//@desc Like a post
+//@access Private
+
+exports.likePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "server error" });
+  }
+};
+
+//@route PUT api/posts/unlike/:id
+//@desc unlike a post if you liked it before
+//@access Private
+
+exports.unlikePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post has not been liked yet" });
+    }
+    //get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.status(200).json({
+      status: "success",
+      data: { post },
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "server error" });
+  }
+};
+
+//TODO:
+//comments
+
+//@route POST api/post/comment/:id
+//@desc Comment on a post
+//@access Private
+
+exports.addComment = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    const post = await Post.findById(req.params.id);
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    };
+    post.comments.unshift(newComment);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+//@route DELETE api/posts/comment/:id/:comment_id
+//@desc Comment on a post
+//@access Private
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    //Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+
+    //check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User is not authorized" });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+};
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,4 +20,18 @@ router.post(
 router.get("/", auth, postController.getPosts);
 router.get("/:id", auth, postController.getSinglePost);
 router.delete("/:id", auth, postController.deletePost);
+
+//like buttons @route PUT api/posts/like/:id
+router.put("/like/:id", auth, postController.likePost);
+router.put("/unlike/:id", auth, postController.unlikePost);
+
+//comments (adding a comment to our post)
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "text is required").not().isEmpty()]],
+  postController.addComment
+);
+
+router.delete("/comment/:id/:comment_id", auth, postController.deleteComment);
+
 module.exports = router;
