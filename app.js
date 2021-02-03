726490a4e04b4c4a16ee70bd6ba6038ecbbf0b07
Askar Talantbek <murutchan@gmail.com>
2021-02-03 13:45:16 -0600

    production

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const path = require("path");
+// const express = require("express");
+// const morgan = require("morgan");
+// const cors = require("cors");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-//init MiddleWare
-app.use(express.json({ extended: false }));
-app.use(morgan("dev"));
-app.use(cors());
-app.use;
+// //init MiddleWare
+// app.use(express.json({ extended: false }));
+// app.use(morgan("dev"));
+// app.use(cors());
+// app.use;
 
-//Define routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
+// //Define routes
+// app.use("/api/users", require("./routes/api/users"));
+// app.use("/api/auth", require("./routes/api/auth"));
+// app.use("/api/profile", require("./routes/api/profile"));
+// app.use("/api/posts", require("./routes/api/posts"));
 
-//SERVE STATIC ASSETS IN PRODUCTION
-if (process.env.NODE_ENV === "production") {
-  //SET static folder
-  app.use(express.static("client/build"));
+// //SERVE STATIC ASSETS IN PRODUCTION
+// if (process.env.NODE_ENV === "production") {
+//   //SET static folder
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
-module.exports = app;
+// module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const app = require("./app");
 
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
 //Connect database
 connectDB();
+
+//init MiddleWare
+app.use(express.json({ extended: false }));
+app.use(morgan("dev"));
+app.use(cors());
+app.use;
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+//SERVE STATIC ASSETS IN PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  //SET static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
