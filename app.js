b0a7bb272ee71701179a2871f41874418dade0b3
Askar Talantbek <murutchan@gmail.com>
2021-02-03 13:32:25 -0600

    Prepared for deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -10,12 +11,20 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use;
 
-app.get("/", (req, res) => res.send("API running"));
-
 //Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//SERVE STATIC ASSETS IN PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  //SET static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
