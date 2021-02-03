a8439167535d0f555dc7187d4cbf4512d314c161
Askar Talantbek <murutchan@gmail.com>
2021-02-03 09:55:28 -0600

    post item

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/posts";
+
 import "./App.css";
 
 if (localStorage.token) {
@@ -62,6 +64,7 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <PrivateRoute exact path="/posts" component={Posts} />
             </Switch>
           </section>
         </Fragment>
