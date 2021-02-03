6df23abbd6013703164c0dfb9cf2c0c399a307b0
Askar Talantbek <murutchan@gmail.com>
2021-02-03 10:40:41 -0600

    delete post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 //GET POSTS
 export const getPosts = () => async (dispatch) => {
@@ -44,6 +44,23 @@ export const removeLike = (id) => async (dispatch) => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response, status: err.response },
+    });
+  }
+};
+
+//delete post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`http://localhost:5000/api/posts/${id}`);
+    dispatch({
+      type: DELETE_POST,
+      payload: { id },
+    });
+    dispatch(setAlert("Post removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
